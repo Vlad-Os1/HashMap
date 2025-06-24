04e32d663fdc9b918d7cf8d397b96ba561b6f6b0
Vlad_Os <oshovskiyvlad28@gmail.com>
2025-06-24 22:00:46 +0300

    feat: implement HashMap with fundamental methods - create buckets using Array.from to create an array of empty arrays - implement private method hash to compute index based on key - implement set method:   - updates value if key exists   - otherwise adds new [key, value] pair to the bucket   - triggers _resize log when load factor exceeds 0.75 - implement get method to retrieve value by key - implement has method to check if key exists - implement remove method to delete key-value pair - add placeholder _resize method to log a message for future resizing logic

---
+++ index.js
@@ -0,0 +1,82 @@
+class HashMap {
+  constructor(size = 16) {
+    this.buckets = Array.from({ length: size }, () => []);
+    this.size = size;
+    this.count = 0; // to track the load factor
+  }
+
+  _hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = hashCode % this.size;
+    }
+    return hashCode;
+  }
+  set(key, value) {
+    let index = this._hash(key);
+    let bucket = this.buckets[index];
+
+    for (let pair of bucket) {
+      if (pair[0] === key) {
+        pair[1] = value;
+        return this.buckets;
+      }
+    }
+    bucket.push([key, value]);
+    this.count++;
+    if (this.count / this.size >= 0.75) {
+      this._resize();
+    }
+    return this.buckets;
+  }
+
+  get(key) {
+    let index = this._hash(key);
+    let bucket = this.buckets[index];
+
+    for (let pair of bucket) {
+      if (pair[0] === key) {
+        return pair[1];
+      }
+    }
+    return null;
+  }
+
+  has(key) {
+    let index = this._hash(key);
+    let bucket = this.buckets[index];
+    for (let pair of bucket) {
+      if (pair[0] === key) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  remove(key) {
+    let index = this._hash(key);
+    let bucket = this.buckets[index];
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket.splice(i, 1);
+        this.count--;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  _resize() {
+    console.log('i am pretty BIG NOW :)');
+  }
+}
+
+let hm = new HashMap(16);
+console.log(hm.set('rock', 'stone'));
+console.log(hm.set('rock', 'and stone'));
+console.log(hm.set('WW', 'Say my name'));
+console.log(hm.remove('WW'));
+console.log(hm.buckets);
